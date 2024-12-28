421dc033252c15bf7dc1d709acfbbcf7a913ab7b
alaa <alaa.elkeshky33@gmail.com>
2024-12-28 22:13:30 +0200

    finish app logic

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -28,154 +28,157 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.centerRight,
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  screenResult,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 48,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    screenResult,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 48,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomButton(
-                      label: 'AC',
-                      color: Color(0xff616161),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '⌫',
-                      color: Color(0xff616161),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '/',
-                      color: Color(0xff005DB2),
-                      onClick: onOperatorClicked
-                  ),
-                  CustomButton(
-                      label: '*',
-                      color: Color(0xff005DB2),
-                      onClick: onOperatorClicked),
-                ],
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomButton(
+                        label: 'AC',
+                        color: Color(0xff616161),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '⌫',
+                        color: Color(0xff616161),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '/',
+                        color: Color(0xff005DB2),
+                        onClick: onOperatorClicked
+                    ),
+                    CustomButton(
+                        label: '*',
+                        color: Color(0xff005DB2),
+                        onClick: onOperatorClicked),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomButton(
-                      label: '7',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '8',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '9',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '-',
-                      color: Color(0xff005DB2),
-                      onClick: onOperatorClicked),
-                ],
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomButton(
+                        label: '7',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '8',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '9',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '-',
+                        color: Color(0xff005DB2),
+                        onClick: onOperatorClicked),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomButton(
-                      label: '4',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '5',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '6',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '+',
-                      color: Color(0xff005DB2),
-                      onClick: onOperatorClicked
-                  ),
-                ],
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomButton(
+                        label: '4',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '5',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '6',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '+',
+                        color: Color(0xff005DB2),
+                        onClick: onOperatorClicked
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomButton(
-                      label: '1',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '2',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '3',
-                      color: Color(0xff303136),
-                      onClick: onBtnClicked),
-                  CustomButton(
-                      label: '.',
-                      color: Color(0xff005DB2),
-                      onClick: onBtnClicked),
-                ],
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomButton(
+                        label: '1',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '2',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '3',
+                        color: Color(0xff303136),
+                        onClick: onBtnClicked),
+                    CustomButton(
+                        label: '.',
+                        color: Color(0xff005DB2),
+                        onClick: onBtnClicked),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          onEqualClicked('=');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xff303136),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            onEqualClicked('=');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xff303136),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
                           ),
-                        ),
-                        child: const Text(
-                          "=",
-                          style: TextStyle(
-                            fontSize: 32,
-                            color: Color(0xFF1991FF),
+                          child: const Text(
+                            "=",
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Color(0xFF1991FF),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  CustomButton(
-                    label: '0',
-                    color: Color(0xff005DB2),
-                    onClick: onBtnClicked,
-                  ),
-                ],
+                    CustomButton(
+                      label: '0',
+                      color: Color(0xff005DB2),
+                      onClick: onBtnClicked,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
